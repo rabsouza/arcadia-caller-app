b6d1b72c9f030a0928a4404007972ca79bc12259
Rafael Batista <rabsouza@gmail.com>
2016-07-14 00:23:10 -0300

    [rabsouza] Ajuste para context mock e prod

--- generatedPackage.sh
+++ generatedPackage.sh
@@ -7,10 +7,10 @@ echo "Build to app"
 gradle :app:assembleDebug
 
 echo "Copy to apk file"
-cp ./app/build/outputs/apk/app-debug.apk ./app/build/outputs/apk/$apk_name
+cp ./app/build/outputs/apk/app-prod-debug.apk ./app/build/outputs/apk/$apk_name
 
 echo "Add apk to git"
-cp ./app/build/outputs/apk/app-debug.apk ./$apk_name
+cp ./app/build/outputs/apk/app-prod-debug.apk ./$apk_name
 git add --force ./$apk_name
 git ci -m "[shell] Generate new version $apk_name"
 
