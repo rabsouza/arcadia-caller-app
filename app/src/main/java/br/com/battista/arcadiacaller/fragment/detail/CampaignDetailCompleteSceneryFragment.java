8b8929ac33791b2323a546340b47769702299bb8
Rafael Batista <rabsouza@gmail.com>
2016-07-30 20:19:42 -0300

    [rabsouza] Implementa finalizar cenario e melhorias de layout

--- app/src/main/java/br/com/battista/arcadiacaller/fragment/detail/CampaignDetailCompleteSceneryFragment.java
+++ app/src/main/java/br/com/battista/arcadiacaller/fragment/detail/CampaignDetailCompleteSceneryFragment.java
@@ -18,9 +18,11 @@ import android.widget.TextView;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.google.common.base.Strings;
+import com.google.common.collect.Maps;
 import com.weiwangcn.betterspinner.library.material.MaterialBetterSpinner;
 
 import java.text.MessageFormat;
+import java.util.Map;
 
 import br.com.battista.arcadiacaller.R;
 import br.com.battista.arcadiacaller.constants.BundleConstant;
@@ -28,6 +30,7 @@ import br.com.battista.arcadiacaller.fragment.BaseFragment;
 import br.com.battista.arcadiacaller.model.Campaign;
 import br.com.battista.arcadiacaller.model.Scenery;
 import br.com.battista.arcadiacaller.model.SceneryCampaign;
+import br.com.battista.arcadiacaller.model.enuns.NameGuildEnum;
 import br.com.battista.arcadiacaller.util.AndroidUtils;
 
 public class CampaignDetailCompleteSceneryFragment extends BaseFragment {
@@ -43,7 +46,46 @@ public class CampaignDetailCompleteSceneryFragment extends BaseFragment {
     private TextView txtWonTitleScenery;
     private MaterialBetterSpinner spnGuildWinner;
 
+    private Map<NameGuildEnum, Integer[]> guildKeys = Maps.newHashMap();
+
     public CampaignDetailCompleteSceneryFragment() {
+
+        NameGuildEnum nameGuildBlue = NameGuildEnum.BLUE;
+        Integer keysBlue[] = {
+                R.id.detail_card_view_complete_scenery_result_guild_img_blue,
+                R.id.detail_card_view_scenery_least_deaths_blue,
+                R.id.detail_card_view_scenery_most_coins_blue,
+                R.id.detail_card_view_scenery_won_reward_blue,
+                R.id.detail_card_view_scenery_won_title_blue};
+        guildKeys.put(nameGuildBlue, keysBlue);
+
+        NameGuildEnum nameGuildGrenn = NameGuildEnum.GREEN;
+        Integer keysGrenn[] = {
+                R.id.detail_card_view_complete_scenery_result_guild_img_green,
+                R.id.detail_card_view_scenery_least_deaths_green,
+                R.id.detail_card_view_scenery_most_coins_green,
+                R.id.detail_card_view_scenery_won_reward_green,
+                R.id.detail_card_view_scenery_won_title_green};
+        guildKeys.put(nameGuildGrenn, keysGrenn);
+
+        NameGuildEnum nameGuildRed = NameGuildEnum.RED;
+        Integer keysRed[] = {
+                R.id.detail_card_view_complete_scenery_result_guild_img_red,
+                R.id.detail_card_view_scenery_least_deaths_red,
+                R.id.detail_card_view_scenery_most_coins_red,
+                R.id.detail_card_view_scenery_won_reward_red,
+                R.id.detail_card_view_scenery_won_title_red};
+        guildKeys.put(nameGuildRed, keysRed);
+
+        NameGuildEnum nameGuildOrange = NameGuildEnum.ORANGE;
+        Integer keysOrange[] = {
+                R.id.detail_card_view_complete_scenery_result_guild_img_orange,
+                R.id.detail_card_view_scenery_least_deaths_orange,
+                R.id.detail_card_view_scenery_most_coins_orange,
+                R.id.detail_card_view_scenery_won_reward_orange,
+                R.id.detail_card_view_scenery_won_title_orange};
+        guildKeys.put(nameGuildOrange, keysOrange);
+
     }
 
     public static CampaignDetailCompleteSceneryFragment newInstance(Campaign campaign) {
@@ -93,45 +135,84 @@ public class CampaignDetailCompleteSceneryFragment extends BaseFragment {
 
             SceneryCampaign sceneryCurrent = campaign.getSceneryCurrent();
             Scenery scenery = sceneryCurrent.getScenery();
-            if (scenery != null) {
-                Log.i(TAG, "processDataFragment: Fill current scenery!");
-
-                txtLocationScenery = (TextView) viewFragment.findViewById(R.id.detail_card_view_scenery_location);
-                String sceneryLocation = String.valueOf(getContext().getText(R.string.hint_scenery_head));
-                CharSequence difficulty = getContext().getText(scenery.getDifficulty().getDescRes());
-                CharSequence locationText = getContext().getText(scenery.getLocation().getDescRes());
-                txtLocationScenery.setText(MessageFormat.format(sceneryLocation, difficulty, locationText));
-
-                txtNameScenery = (TextView) viewFragment.findViewById(R.id.detail_card_view_scenery_name);
-                txtNameScenery.setText(scenery.getName());
-
-                txtWonRewardScenery = (TextView) viewFragment.findViewById(R.id.detail_card_view_scenery_won_reward);
-                if (scenery.getWonReward() != null) {
-                    txtWonRewardScenery.setText(scenery.getWonReward().getName());
-                } else {
-                    txtWonRewardScenery.setText(R.string.none);
-                }
-
-                txtWonTitleScenery = (TextView) viewFragment.findViewById(R.id.detail_card_view_scenery_won_title);
-                if (!Strings.isNullOrEmpty(scenery.getWonTitle())) {
-                    txtWonTitleScenery.setText(scenery.getWonTitle());
-                } else {
-                    txtWonTitleScenery.setText(R.string.none);
-                }
-            }
-
+            loadDataScenery(viewFragment, scenery);
             loadGuildsImg(viewFragment);
-
-            ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getContext(),
-                    android.R.layout.simple_dropdown_item_1line, campaign.getAllActiveGuilds());
-            spnGuildWinner = (MaterialBetterSpinner)
-                    viewFragment.findViewById(R.id.detail_card_view_scenery_winner);
-            spnGuildWinner.setAdapter(arrayAdapter);
-            spnGuildWinner.setText(R.string.none);
+            loadGuildsWinner(viewFragment);
+            showGuildsActive(viewFragment, campaign);
         }
     }
 
+    private void showGuildsActive(View viewFragment, Campaign campaign) {
+        Log.i(TAG, "showGuildsActive: Show the view elements of guild active!");
+        if (Strings.isNullOrEmpty(campaign.getGuild01()) || campaign.getHeroesGuild01() == null) {
+            for (Integer viewGuildResId : guildKeys.get(NameGuildEnum.BLUE)) {
+                viewFragment.findViewById(viewGuildResId).setVisibility(View.GONE);
+            }
+        }
+
+        if (Strings.isNullOrEmpty(campaign.getGuild02()) || campaign.getHeroesGuild02() == null) {
+            for (Integer viewGuildResId : guildKeys.get(NameGuildEnum.GREEN)) {
+                viewFragment.findViewById(viewGuildResId).setVisibility(View.GONE);
+            }
+        }
+
+        if (Strings.isNullOrEmpty(campaign.getGuild03()) || campaign.getHeroesGuild03() == null) {
+            for (Integer viewGuildResId : guildKeys.get(NameGuildEnum.RED)) {
+                viewFragment.findViewById(viewGuildResId).setVisibility(View.GONE);
+            }
+        }
+
+        if (Strings.isNullOrEmpty(campaign.getGuild04()) || campaign.getHeroesGuild04() == null) {
+            for (Integer viewGuildResId : guildKeys.get(NameGuildEnum.ORANGE)) {
+                viewFragment.findViewById(viewGuildResId).setVisibility(View.GONE);
+            }
+        }
+    }
+
+    private void loadDataScenery(View viewFragment, Scenery scenery) {
+        if (scenery != null) {
+            Log.i(TAG, "processDataFragment: Fill current scenery!");
+
+            txtLocationScenery = (TextView) viewFragment.findViewById(R.id.detail_card_view_scenery_location);
+            String sceneryLocation = String.valueOf(getContext().getText(R.string.hint_scenery_head));
+            CharSequence difficulty = getContext().getText(scenery.getDifficulty().getDescRes());
+            CharSequence locationText = getContext().getText(scenery.getLocation().getDescRes());
+            txtLocationScenery.setText(MessageFormat.format(sceneryLocation, difficulty, locationText));
+
+            txtNameScenery = (TextView) viewFragment.findViewById(R.id.detail_card_view_scenery_name);
+            txtNameScenery.setText(scenery.getName());
+
+            txtWonRewardScenery = (TextView) viewFragment.findViewById(R.id.detail_card_view_scenery_won_reward);
+            if (scenery.getWonReward() != null) {
+                txtWonRewardScenery.setText(scenery.getWonReward().getName());
+            } else {
+                txtWonRewardScenery.setText(R.string.none);
+                (viewFragment.findViewById(R.id.detail_card_view_scenery_won_reward_group)).setVisibility(View.GONE);
+            }
+
+            txtWonTitleScenery = (TextView) viewFragment.findViewById(R.id.detail_card_view_scenery_won_title);
+            if (!Strings.isNullOrEmpty(scenery.getWonTitle())) {
+                txtWonTitleScenery.setText(scenery.getWonTitle());
+            } else {
+                txtWonTitleScenery.setText(R.string.none);
+                (viewFragment.findViewById(R.id.detail_card_view_scenery_won_title_group)).setVisibility(View.GONE);
+            }
+        }
+    }
+
+    private void loadGuildsWinner(View viewFragment) {
+        Log.i(TAG, "loadGuildsWinner: Load all guilds active for winner!");
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getContext(),
+                android.R.layout.simple_dropdown_item_1line, campaign.getAllActiveGuilds());
+        spnGuildWinner = (MaterialBetterSpinner)
+                viewFragment.findViewById(R.id.detail_card_view_scenery_winner);
+        spnGuildWinner.setAdapter(arrayAdapter);
+        spnGuildWinner.setText(R.string.none);
+    }
+
     private void loadGuildsImg(View viewFragment) {
+        Log.i(TAG, "loadGuildsImg: Load guilds imgs!");
         Glide.with(getContext())
                 .load(BLUE.getUrlImg())
                 .diskCacheStrategy(DiskCacheStrategy.RESULT)
