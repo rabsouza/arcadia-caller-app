d240d96ec2de56b1398a9187a079b1a2b3eaadc1
Rafael Batista <rafaelbs@ciandt.com>
2016-08-02 18:05:03 -0300

    [rabsouza] Ajuste na versao do gradle e no flayer da mock

--- app/src/mock/java/br/com/battista/arcadiacaller/Inject.java
+++ app/src/mock/java/br/com/battista/arcadiacaller/Inject.java
@@ -1,11 +1,51 @@
 package br.com.battista.arcadiacaller;
 
 import br.com.battista.arcadiacaller.service.AppService;
-import br.com.battista.arcadiacaller.service.FakeAppService;
+import br.com.battista.arcadiacaller.service.CampaignService;
+import br.com.battista.arcadiacaller.service.CampainhaCompleteService;
+import br.com.battista.arcadiacaller.service.CardService;
+import br.com.battista.arcadiacaller.service.HeroService;
+import br.com.battista.arcadiacaller.service.LoginService;
+import br.com.battista.arcadiacaller.service.SceneryService;
+import br.com.battista.arcadiacaller.service.StatisticUserService;
+import br.com.battista.arcadiacaller.service.UserService;
 
 public class Inject {
 
     public synchronized static AppService provideAppService() {
-        return new FakeAppService();
+        return new AppService();
     }
+
+    public synchronized static LoginService provideLoginService() {
+        return new LoginService();
+    }
+
+    public synchronized static UserService provideUserService() {
+        return new UserService();
+    }
+
+    public synchronized static HeroService provideHeroService() {
+        return new HeroService();
+    }
+
+    public synchronized static CardService provideCardService() {
+        return new CardService();
+    }
+
+    public synchronized static SceneryService provideSceneryService() {
+        return new SceneryService();
+    }
+
+    public synchronized static CampaignService provideCampaignService() {
+        return new CampaignService();
+    }
+
+    public synchronized static StatisticUserService provideStatisticUserService() {
+        return new StatisticUserService();
+    }
+
+    public synchronized static CampainhaCompleteService provideCampainhaCompleteService() {
+        return new CampainhaCompleteService();
+    }
+
 }
